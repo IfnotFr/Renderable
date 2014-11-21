793a16566568ca1add72878958ca7dc2da971531
Anael Favre <a.favre@web-6.fr>
2014-11-21 11:13:41 +0100

    Fix image file creation with attributes from array

--- src/views/renderer/attribute/image.blade.php
+++ src/views/renderer/attribute/image.blade.php
@@ -1,8 +1,5 @@
 <img
-    src="{{ asset('six/img/source/' . $block->source) }}"
-    {{ isset($block->alt) ? 'alt="' . $block->alt . '"' : '' }}
-    class="{{ isset($block->responsive) ? 'img-responsive' : '' }} {{ isset($block->shape) ? 'img-' . $block->shape : '' }}"
-    style="
-        {{ isset($block->width) ? 'max-width: ' . $block->width . 'px;' : '' }}
-        {{ isset($block->height) ? 'max-height: ' . $block->height . 'px;' : '' }}"
+    @foreach($value as $attr => $value)
+        {{ $attr . '=' . '"' . $value . '"' }}
+    @endforeach
 />
